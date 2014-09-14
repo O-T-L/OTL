063ba665eb8809d4fd519348d56af4ed2cb89054
Ruimin Shen (申瑞珉) <ruimin0shen@gmail.com>
2014-09-14 17:57:43 +0800

    K-Means

---
+++ OptimizationTemplateLibrary/Include/OTL/Utility/Clustering/KMeans.h
@@ -0,0 +1,124 @@
+/*!
+Copyright (C) 2014, 申瑞珉 (Ruimin Shen)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <cassert>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <limits>
+
+namespace otl
+{
+namespace utility
+{
+namespace clustering
+{
+template <typename _TPoint>
+size_t NearestCentroid(const _TPoint &point, const std::vector<_TPoint> &centroids)
+{
+	typedef typename _TPoint::value_type _TReal;
+	assert(centroids.size() > 0);
+	size_t index = 0;
+	_TReal distance = sqrt(std::inner_product(point.begin(), point.end(), centroids[index].begin(), (_TReal)0
+		, std::plus<_TReal>()
+		, [](_TReal x, _TReal y)->_TReal{_TReal t = x - y;return t * t;}
+	));
+	for (size_t i = 1; i < centroids.size(); ++i)
+	{
+		_TReal _distance = sqrt(std::inner_product(point.begin(), point.end(), centroids[i].begin(), (_TReal)0
+			, std::plus<_TReal>()
+			, [](_TReal x, _TReal y)->_TReal{_TReal t = x - y;return t * t;}
+		));
+		if (_distance < distance)
+		{
+			distance = _distance;
+			index = i;
+		}
+	}
+	return index;
+}
+
+template <typename _TIterator>
+typename std::iterator_traits<typename std::iterator_traits<_TIterator>::value_type>::value_type CalculateCentroid(_TIterator begin, _TIterator end)
+{
+	typedef typename std::iterator_traits<_TIterator>::value_type _TPointer;
+	typedef typename std::iterator_traits<_TPointer>::value_type _TPoint;
+	typedef typename _TPoint::value_type _TReal;
+	_TPoint centroid = **begin;
+	for (size_t n = 0; n < centroid.size(); ++n)
+	{
+		size_t count = 1;
+		for (_TIterator i = ++begin; i != end; ++i)
+		{
+			centroid[n] += (**i)[n];
+			++count;
+		}
+		centroid[n] /= count;
+	}
+	return centroid;
+}
+
+template <typename _TIterator, typename _TPoint>
+std::vector<std::list<const _TPoint *> > KMeans(_TIterator begin, _TIterator end, std::vector<_TPoint> &centroids, size_t iteration = -1)
+{
+	typedef const _TPoint * _TPointer;
+	typedef typename _TPoint::value_type _TReal;
+	assert(centroids.size() > 0);
+	std::vector<std::list<_TPointer> > clusters(centroids.size());
+	{//Initialize
+		assert(std::distance(begin, end) >= centroids.size());
+		_TIterator src = begin;
+		for (size_t n = 0; n < clusters.size(); ++n)
+		{
+			_TPointer pointer = *src;
+			centroids[n] = *pointer;
+			clusters[n].push_back(pointer);
+			++src;
+		}
+		while (src != end)
+		{
+			_TPointer pointer = *src;
+			const size_t index = NearestCentroid(*pointer, centroids);
+			clusters[index].push_back(pointer);
+			++src;
+		}
+	}
+	while (iteration)
+	{
+		std::vector<_TPoint> _centroids(centroids.size());
+		for (size_t n = 0; n < clusters.size(); ++n)
+			_centroids[n] = CalculateCentroid(clusters[n].begin(), clusters[n].end());
+		if (_centroids == centroids)
+			break;
+		centroids = _centroids;
+		for (size_t n = 0; n < clusters.size(); ++n)
+			clusters[n].clear();
+		for (_TIterator i = begin; i != end; ++i)
+		{
+			_TPointer pointer = *i;
+			const size_t index = NearestCentroid(*pointer, centroids);
+			clusters[index].push_back(pointer);
+		}
+		--iteration;
+	}
+	return clusters;
+}
+}
+}
+}
---
+++ OptimizationTemplateLibrary/Tests/Utility/KMeans.cpp
@@ -0,0 +1,63 @@
+/*!
+Copyright (C) 2014, 申瑞珉 (Ruimin Shen)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cmath>
+#include <vector>
+#include <list>
+#include <random>
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <OTL/Utility/Clustering/KMeans.h>
+
+namespace clustering
+{
+BOOST_AUTO_TEST_CASE(TestK_Means)
+{
+	typedef std::mt19937 _TRandom;
+	typedef double _TReal;
+	typedef std::vector<_TReal> _TPoint;
+	typedef const _TPoint * _TConstPointer;
+	_TRandom random;
+	const size_t nPoints = 100;
+	std::vector<_TPoint> points(nPoints);
+	std::vector<_TConstPointer> _points(points.size());
+	for (size_t i = 0; i < points.size(); ++i)
+	{
+		_TPoint &point = points[i];
+		point.resize(3);
+		std::uniform_real_distribution<_TReal> dist;
+		for (size_t j = 0; j < point.size(); ++j)
+			point[j] = dist(random);
+		_points[i] = &point;
+	}
+	std::random_shuffle(_points.begin(), _points.end());
+	std::vector<_TPoint> centroids(5);
+	std::vector<std::list<_TConstPointer> > clusters = otl::utility::clustering::KMeans(_points.begin(), _points.end(), centroids);
+	BOOST_CHECK_EQUAL(clusters.size(), centroids.size());
+	{
+		std::list<_TConstPointer> merge;
+		for (size_t index = 0; index < clusters.size(); ++index)
+		{
+			std::list<_TConstPointer> &cluster = clusters[index];
+			for (auto i = cluster.begin(); i != cluster.end(); ++i)
+				BOOST_CHECK(std::find(merge.begin(), merge.end(), *i) == merge.end());
+			merge.splice(merge.end(), cluster, cluster.begin(), cluster.end());
+		}
+		BOOST_CHECK_EQUAL(merge.size(), nPoints);
+	}
+}
+}
