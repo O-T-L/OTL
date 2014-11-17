0e5963c94721c0cb20257845d1278de160be12fa
柏卉 (Hui Bai) <hui7bai@gmail.com>
2014-11-17 13:06:14 +0800

    Better design

--- OptimizationTemplateLibrary/Include/OTL/Optimizer/SPEA2/RawFitness.h
+++ OptimizationTemplateLibrary/Include/OTL/Optimizer/SPEA2/RawFitness.h
@@ -20,11 +20,11 @@ namespace spea2
  * \param[in] dominate The dominance function
  * \return The number of solutions individual dominates
  */
-template <typename _TIterator, typename _TDominate>
-size_t StrengthFitness(_TIterator individual, _TIterator begin, _TIterator end, _TDominate dominate)
+template <typename _TIterator1, typename _TIterator2, typename _TDominate>
+size_t StrengthFitness(_TIterator1 individual, _TIterator2 begin, _TIterator2 end, _TDominate dominate)
 {
 	size_t count = 0;
-	for (_TIterator i = begin; i != end; ++i)
+	for (_TIterator2 i = begin; i != end; ++i)
 	{
 		if (i != individual)
 		{
@@ -43,11 +43,11 @@ size_t StrengthFitness(_TIterator individual, _TIterator begin, _TIterator end, 
  * \param[in] dominate The dominance function
  * \return The sum of the strength value of individual's dominators
  */
-template <typename _TIterator, typename _TDominate>
-size_t RawFitness(_TIterator individual, _TIterator begin, _TIterator end, _TDominate dominate)
+template <typename _TIterator1, typename _TIterator2, typename _TDominate>
+size_t RawFitness(_TIterator1 individual, _TIterator2 begin, _TIterator2 end, _TDominate dominate)
 {
 	size_t rawFitness = 0;
-	for (_TIterator dominator = begin; dominator != end; ++dominator)
+	for (_TIterator2 dominator = begin; dominator != end; ++dominator)
 	{
 		if (dominator != individual)
 		{
