5752af0b425013704181f53315899ffc51dbe0e2
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2015-03-09 12:46:23 +0800

    Fixing R-NSGA-II

--- OptimizationTemplateLibrary/Include/OTL/Optimizer/R-NSGA-II/R-NSGA-II.h
+++ OptimizationTemplateLibrary/Include/OTL/Optimizer/R-NSGA-II/R-NSGA-II.h
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <OTL/Optimizer/NSGA-II/Offspring.h>
 #include <OTL/Optimizer/NSGA-II/CrowdingDistanceAssignment.h>
 #include <OTL/Optimizer/NSGA-II/Individual.h>
-#include "R-Dominance.h"
 
 namespace otl
 {
@@ -52,14 +51,19 @@ public:
 	typedef typename otl::crossover::WithCrossover<TReal, TDecision>::TCrossover TCrossover;
 	typedef typename otl::mutation::WithMutation<TReal, TDecision>::TMutation TMutation;
 
-	R_NSGA_II(TRandom random, TProblem &problem, const std::vector<TDecision> &initial, TCrossover &crossover, TMutation &mutation, const std::vector<TReal> &referencePoint, const TReal threshold);
+	R_NSGA_II(TRandom random, TProblem &problem, const std::vector<TDecision> &initial, TCrossover &crossover, TMutation &mutation, const std::vector<TReal> &referencePoint, const std::vector<TReal> &weight, const TReal threshold);
 	~R_NSGA_II(void);
 	const std::vector<TReal> &GetReferencePoint(void) const;
+	const std::vector<TReal> &GetWeight(void) const;
 	TReal GetThreshold(void) const;
 	TSolutionSet MakeOffspring(const TSolutionSet &ancestor);
+	bool Dominate(const std::vector<TReal> &point1, const std::vector<TReal> &point2) const;
 	bool Dominate(const TIndividual &individual1, const TIndividual &individual2) const;
+	TReal CalculateDist(const std::vector<TReal> &objective) const;
 
 protected:
+	template <typename _TIterator> void _CalculateObjRange(_TIterator begin, _TIterator end);
+	template <typename _TIterator> void _CalculateDistRange(_TIterator begin, _TIterator end);
 	void _DoStep(void);
 	template <typename _TPointer, typename _TIterator> static _TIterator _SelectNoncritical(const std::list<_TPointer> &front, _TIterator begin, _TIterator end);
 	template <typename _TPointer, typename _TIterator> static _TIterator _SelectCritical(const std::list<_TPointer> &front, _TIterator begin, _TIterator end);
@@ -67,18 +71,22 @@ protected:
 
 private:
 	std::vector<TReal> referencePoint_;
+	std::vector<TReal> weight_;
 	TReal threshold_;
+	std::vector<TReal> objRange_;
 	TReal distRange_;
 };
 
 template <typename _TReal, typename _TDecision, typename _TRandom>
-R_NSGA_II<_TReal, _TDecision, _TRandom>::R_NSGA_II(TRandom random, TProblem &problem, const std::vector<TDecision> &initial, TCrossover &crossover, TMutation &mutation, const std::vector<TReal> &referencePoint, const TReal threshold)
+R_NSGA_II<_TReal, _TDecision, _TRandom>::R_NSGA_II(TRandom random, TProblem &problem, const std::vector<TDecision> &initial, TCrossover &crossover, TMutation &mutation, const std::vector<TReal> &referencePoint, const std::vector<TReal> &weight, const TReal threshold)
 	: TSuper(problem)
 	, otl::utility::WithRandom<TRandom>(random)
 	, otl::crossover::WithCrossover<TReal, TDecision>(crossover)
 	, otl::mutation::WithMutation<TReal, TDecision>(mutation)
 	, referencePoint_(referencePoint)
+	, weight_(weight)
 	, threshold_(threshold)
+	, objRange_(problem.GetNumberOfObjectives())
 {
 	TSuper::solutionSet_.resize(initial.size());
 	for (size_t i = 0; i < initial.size(); ++i)
@@ -92,7 +100,8 @@ R_NSGA_II<_TReal, _TDecision, _TRandom>::R_NSGA_II(TRandom random, TProblem &pro
 	std::list<_TPointer> solutionSet;
 	for (size_t i = 0; i < TSuper::solutionSet_.size(); ++i)
 		solutionSet.push_back(&TSuper::solutionSet_[i]);
-	distRange_ = CalculateDistRange(referencePoint_, solutionSet.begin(), solutionSet.end());
+	_CalculateObjRange(solutionSet.begin(), solutionSet.end());
+	_CalculateDistRange(solutionSet.begin(), solutionSet.end());
 	while (!solutionSet.empty())
 	{
 		std::list<_TPointer> nondominate = otl::utility::ExtractNondominate(solutionSet, [this](const TIndividual *individual1, const TIndividual *individual2)->bool{return this->Dominate(*individual1, *individual2);});
@@ -110,6 +119,12 @@ template <typename _TReal, typename _TDecision, typename _TRandom>
 const std::vector<_TReal> &R_NSGA_II<_TReal, _TDecision, _TRandom>::GetReferencePoint(void) const
 {
 	return referencePoint_;
+}
+
+template <typename _TReal, typename _TDecision, typename _TRandom>
+const std::vector<_TReal> &R_NSGA_II<_TReal, _TDecision, _TRandom>::GetWeight(void) const
+{
+	return weight_;
 }
 
 template <typename _TReal, typename _TDecision, typename _TRandom>
@@ -134,9 +149,57 @@ typename R_NSGA_II<_TReal, _TDecision, _TRandom>::TSolutionSet R_NSGA_II<_TReal,
 }
 
 template <typename _TReal, typename _TDecision, typename _TRandom>
+bool R_NSGA_II<_TReal, _TDecision, _TRandom>::Dominate(const std::vector<TReal> &point1, const std::vector<TReal> &point2) const
+{
+	assert(point1.size() == point2.size());
+	assert(point1.size() == referencePoint_.size());
+	assert(0 <= threshold_ && threshold_ <= 1);
+	assert(distRange_ >= 0);
+	if (otl::utility::relation::Dominate(point1, point2))
+		return true;
+	else if (otl::utility::relation::Dominate(point2, point1))
+		return false;
+	else
+		return (CalculateDist(point1) - CalculateDist(point2)) / distRange_ < -threshold_;
+
+}
+
+template <typename _TReal, typename _TDecision, typename _TRandom>
 bool R_NSGA_II<_TReal, _TDecision, _TRandom>::Dominate(const TIndividual &individual1, const TIndividual &individual2) const
 {
-	return R_Dominance(individual1.objective_, individual2.objective_, referencePoint_, threshold_, distRange_);
+	return Dominate(individual1.objective_, individual2.objective_);
+}
+
+template <typename _TReal, typename _TDecision, typename _TRandom>
+_TReal R_NSGA_II<_TReal, _TDecision, _TRandom>::CalculateDist(const std::vector<TReal> &objective) const
+{
+	TReal dist = 0;
+	for (size_t i = 0; i < referencePoint_.size(); ++i)
+	{
+		const TReal temp = (objective[i] - referencePoint_[i]) / objRange_[i];
+		dist += temp * temp * weight_[i];
+	}
+	return sqrt(dist);
+}
+
+template <typename _TReal, typename _TDecision, typename _TRandom>
+template <typename _TIterator> void R_NSGA_II<_TReal, _TDecision, _TRandom>::_CalculateObjRange(_TIterator begin, _TIterator end)
+{
+	for (size_t i = 0; i < objRange_.size(); ++i)
+	{
+		auto fMinMax = std::minmax_element(begin, end, [i](const TIndividual *individual1, const TIndividual *individual2)->bool{return individual1->objective_[i] < individual2->objective_[i];});
+		objRange_[i] = (**fMinMax.second).objective_[i] - (**fMinMax.first).objective_[i];
+	}
+}
+
+template <typename _TReal, typename _TDecision, typename _TRandom>
+template <typename _TIterator> void R_NSGA_II<_TReal, _TDecision, _TRandom>::_CalculateDistRange(_TIterator begin, _TIterator end)
+{
+	std::list<TReal> dists;
+	for (_TIterator i = begin; i != end; ++i)
+		dists.push_back(CalculateDist((**i).objective_));
+	auto minMax = std::minmax_element(dists.begin(), dists.end());
+	distRange_ = *minMax.second - *minMax.first;
 }
 
 template <typename _TReal, typename _TDecision, typename _TRandom>
@@ -150,6 +213,8 @@ void R_NSGA_II<_TReal, _TDecision, _TRandom>::_DoStep(void)
 		mix.push_back(&ancestor[i]);
 	for (size_t i = 0; i < offspring.size(); ++i)
 		mix.push_back(&offspring[i]);
+	_CalculateObjRange(mix.begin(), mix.end());
+	_CalculateDistRange(mix.begin(), mix.end());
 	typedef typename TSolutionSet::iterator _TIterator;
 	otl::selection::NondominateSelection(mix, TSuper::solutionSet_.begin(), TSuper::solutionSet_.end()
 		, [this](const TIndividual *individual1, const TIndividual *individual2)->bool{return this->Dominate(*individual1, *individual2);}
