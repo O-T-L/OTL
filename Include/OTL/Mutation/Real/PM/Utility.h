44ac4480db7b1ab8957e9d4d87afb1b8a17150d9
Ruimin Shen <ruimin0shen@gmail.com>
2015-11-01 15:39:53 +0800

    fix

--- Include/OTL/Mutation/Real/PM/Utility.h
+++ Include/OTL/Mutation/Real/PM/Utility.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <cassert>
 #include <cmath>
 #include <random>
+#include <OTL/Utility/Fix/Truncate.h>
 
 namespace otl
 {
